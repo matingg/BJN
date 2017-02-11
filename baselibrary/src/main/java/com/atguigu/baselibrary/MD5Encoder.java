c9dafd5f9ae123345f747175485eb07631587b32
matingg <1291203886@qq.com>
2017-02-11 15:43:53 +0800

    123

---
+++ baselibrary/src/main/java/com/atguigu/baselibrary/MD5Encoder.java
@@ -0,0 +1,24 @@
+package com.atguigu.baselibrary;
+
+import java.security.MessageDigest;
+
+/**
+ * Created by 麻少亭 on 2017/2/10.
+ */
+
+public class MD5Encoder {
+
+    public static String encode(String string) throws Exception {
+        byte[] hash = MessageDigest.getInstance("MD5").digest(string.getBytes("UTF-8"));
+        StringBuilder hex = new StringBuilder(hash.length * 2);
+        for (byte b : hash) {
+            if ((b & 0xFF) < 0x10) {
+                hex.append("0");
+            }
+            hex.append(Integer.toHexString(b & 0xFF));
+        }
+        return hex.toString();
+    }
+}
+
+
